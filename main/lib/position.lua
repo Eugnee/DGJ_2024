ecd3a4891c5c997a1447159ef35a481ff266378d
Eugnee <77396838+Eugnee@users.noreply.github.com>
2024-02-02 15:58:14 +0200

    base for levels

---
+++ main/lib/position.lua
@@ -0,0 +1,29 @@
+local C = require "main.constants"
+
+local position = {}
+
+-- set isometric position
+---@param new_pos vector3
+---@param z_index number
+---@param id string|hash|url|nil
+function position.set_iso_position(new_pos, z_index, id)
+    new_pos.z = z_index - (new_pos.y * C.MIN_STEP)
+    go.set_position(new_pos, id)
+end
+
+---@param obj {correction: vector3}
+---@param message {distance: number, normal: vector3}
+function position.correct_position(obj, message)
+    if message.distance > 0 then
+        local proj = vmath.project(obj.correction,
+            message.normal * message.distance);
+        if (proj < 1) then
+            local comp = (message.distance - message.distance * proj) *
+                message.normal;
+            position.set_iso_position(go.get_position() + comp, C.OBECTS_Z_INDEX)
+            obj.correction = obj.correction + comp;
+        end
+    end
+end
+
+return position
