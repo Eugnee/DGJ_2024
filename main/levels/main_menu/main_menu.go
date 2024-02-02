ce23ff4999c7675bf238f6229d438c41a2d2ac3c
thunderdum <vasilenko.i.d.93@gmail.com>
2024-02-02 12:15:54 +0000

    game_controller, 3 level canvases

---
+++ main/levels/main_menu/main_menu.go
@@ -0,0 +1,31 @@
+components {
+	id: "main_menu"
+	component: "/main/levels/main_menu/main_menu.gui"
+	position {
+	  x: 0.0
+	  y: 0.0
+	  z: 0.0
+	}
+	rotation {
+	  x: 0.0
+	  y: 0.0
+	  z: 0.0
+	  w: 1.0
+	}
+  }
+  components {
+	id: "main_menu_script"
+	component: "/main/levels/main_menu/main_menu.script"
+	position {
+	  x: 0.0
+	  y: 0.0
+	  z: 0.0
+	}
+	rotation {
+	  x: 0.0
+	  y: 0.0
+	  z: 0.0
+	  w: 1.0
+	}
+  }
+  
