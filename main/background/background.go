e85daede65d9f7c450779755dd851a8dce145e76
thunderdum <vasilenko.i.d.93@gmail.com>
2024-02-03 09:59:09 +0000

    Background as go with sprite, swapping farward and backward bg around player ship

---
+++ main/background/background.go
@@ -0,0 +1,35 @@
+components {
+  id: "background"
+  component: "/main/background/background.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bg_sprite_1"
+  type: "sprite"
+  data: "tile_set: \"/main/background_1.atlas\"\n"
+  "default_animation: \"background1-3-export\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
