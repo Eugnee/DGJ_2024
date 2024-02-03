3096d8cb7bc055a276e82de2c7ef8f19cbf34d0e
thunderdum <vasilenko.i.d.93@gmail.com>
2024-02-03 11:05:04 +0000

    Backgroud variations, one static, one moving

--- main/backgrounds/background_0.go
+++ main/backgrounds/background_0.go
@@ -1,6 +1,6 @@
 components {
   id: "background"
-  component: "/main/background/background.script"
+  component: "/main/backgrounds/background_0.script"
   position {
     x: 0.0
     y: 0.0
@@ -16,7 +16,7 @@ components {
 embedded_components {
   id: "bg_sprite_1"
   type: "sprite"
-  data: "tile_set: \"/main/background_1.atlas\"\n"
+  data: "tile_set: \"/main/backgrounds/background_0.atlas\"\n"
   "default_animation: \"background1-3-export\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
---
+++ main/backgrounds/background_3.go
@@ -0,0 +1,35 @@
+components {
+  id: "background_3"
+  component: "/main/backgrounds/background_3.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/backgrounds/background_3.atlas\"\n"
+  "default_animation: \"background1-paralax3\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
