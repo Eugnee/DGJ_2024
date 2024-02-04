360d2d329082e4ffe7f306a30d0ea554dbf194a5
thunderdum <vasilenko.i.d.93@gmail.com>
2024-02-04 12:34:41 +0000

    Test shared table

---
+++ main/shared_table.lua
@@ -0,0 +1,21 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local T = {}
+
+local shared = {}
+
+function T.alter_value(idx, value)
+	shared[idx] = value
+end
+
+function T.get_value(idx)
+	return shared[idx]
+end
+
+function T.add_value(value)
+	table.insert(shared, value)
+end
+
+return T
