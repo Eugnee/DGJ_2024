fa42f467c973caecd6e2361ffa5a2ea0267fc58e
Eugnee <77396838+Eugnee@users.noreply.github.com>
2024-02-05 17:07:18 +0200

    new meteors and bounds moving bounds

--- main/enemies/enemy_bullet.go
+++ main/enemies/enemy_bullet.go
@@ -43,6 +43,7 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"enemy\"\n"
   "mask: \"hero\"\n"
+  "mask: \"meteor\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
